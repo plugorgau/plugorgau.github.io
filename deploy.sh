e4e204ad9b0a46b467ebbb036863e040577ac503
AV person <av@plugav1-2.cassa>
2018-04-12 17:13:46 +0800

    Change deploy location

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Temporary hack to enable automatic deployments
-rsync -a --delete /home/glass/plugorgau.github.io/_site/ /var/www/bayonet.plug.org.au/
-chown -R www-data:www-data /var/www/bayonet.plug.org.au/
+rsync -a --delete /home/glass/plugorgau.github.io/_site/ /var/www/plug.org.au/
+chown -R www-data:www-data /var/www/plug.org.au/
 
