e1dad0750232048f7e8caad28dc974b1afcd7771
AV person <av@plugav1-2.cassa>
2018-04-12 17:28:56 +0800

    Symlink media directory into place

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Temporary hack to enable automatic deployments
 rsync -a --delete /home/glass/plugorgau.github.io/_site/ /var/www/plug.org.au/
+ln -s /srv/efs/sites/plug.org.au/media /var/www/plug.org.au/media
 chown -R www-data:www-data /var/www/plug.org.au/
-
