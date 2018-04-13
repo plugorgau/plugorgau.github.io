54bbfe7648e59148b2f41cd4dc979e69d37c7f5d
Glass Deployment <glass@bayonet.plug.org.au>
2018-04-13 10:51:41 +0000

    Add video symlink

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 # Temporary hack to enable automatic deployments
 rsync -a --delete /home/glass/plugorgau.github.io/_site/ /var/www/plug.org.au/
 ln -s /srv/efs/sites/plug.org.au/media/ /var/www/plug.org.au/media
+ln -s /srv/efs/sites/plug.org.au/media/video /var/www/plug.org.au/video
 chown -R www-data:www-data /var/www/plug.org.au/
